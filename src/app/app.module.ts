454773d387c82f4c274ec8f20f1a2c1ca5be07b5
Antonio Hernandez <ahernandezh.dev@gmail.com>
2023-04-30 17:41:14 -0600

    Fin de la sección. Subir en conjunto con el docs page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 // * Modules
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroesModule],
+  imports: [BrowserModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Injectable({ providedIn: 'root' })
+export class DBZService {
+  public characters: Character[] = [
+    { id: uuid(), name: 'Krillin', power: 1000 },
+    { id: uuid(), name: 'Kokun', power: 9000 },
+    { id: uuid(), name: 'Vegeta', power: 7500 },
+  ];
+
+  addCharacter(character: Character): void {
+    const newCharacter: Character = { id: uuid(), ...character };
+    this.characters.push(newCharacter);
+  }
+
+  // onDeleteCharacter(index: number): void {
+  //   this.characters.splice(index, 1);
+  // }
+
+  deleteCharacterById(id: string): void {
+    this.characters = this.characters.filter(
+      (character) => character.id !== id
+    );
+  }
+}
